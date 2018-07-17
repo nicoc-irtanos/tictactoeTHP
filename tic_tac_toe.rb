fd821d3921370b7243905d3e5966c11023c10c2a
Dyf-Tfh <n.zhao@hotmail.fr>
2018-07-17 11:25:12 +0200

    init boardcase

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -23,12 +23,21 @@ class Board
   attr_accessor :cases
 
   def initialize
+    @boardcase_1 = BoardCase.new
+    @boardcase_2 = BoardCase.new
+    @boardcase_3 = BoardCase.new
+    @boardcase_4 = BoardCase.new
+    @boardcase_5 = BoardCase.new
+    @boardcase_6 = BoardCase.new
+    @boardcase_7 = BoardCase.new
+    @boardcase_8 = BoardCase.new
+    @boardcase_9 = BoardCase.new
     cases = [boardcase_1, boardcase_2, boardcase_3, boardcase_4, boardcase_5, boardcase_6, boardcase_7, boardcase_8, boardcase_9]
   end
 
   def to_s
   #TO DO : afficher le plateau
-
+    puts @boardcase_1 + @boardcase_2 + @boardcase_3 , @boardcase_4 + @boardcase_5 + @boardcase_6, @boardcase_7 + @boardcase_8 + @boardcase_9
   end
 
   def play
@@ -38,6 +47,7 @@ class Board
   def victory?
     #TO DO : qui gagne ?
   end
+end
 
 class Player
   #TO DO : la classe a 2 attr_accessor, son nom, sa valeur (X ou O). Elle a un attr_writer : il a gagné ?
@@ -45,9 +55,10 @@ class Player
   attr_accessor :name, :value #Noms et valeurs
   attr_writer :state #Victoire ou défaite
 
-  def initialize
+  def initialize(value)
     puts "Set your name:"
-    name = gets.chomp
+    @name = gets.chomp
+    @value = value
   end
 end
 
@@ -55,8 +66,8 @@ class Game
   def initialize
     #TO DO : créé 2 joueurs, créé un board
     puts "Welcome to the Tic tac toe game !"
-    @player1 = Player.new
-    @player2 = Player.new
+    @player1 = Player.new("X")
+    @player2 = Player.new("O")
     @board = Board.new
   end
 
@@ -68,7 +79,6 @@ class Game
     #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
   end
 
-
 end
 
-Game.new.go
+Game.new
