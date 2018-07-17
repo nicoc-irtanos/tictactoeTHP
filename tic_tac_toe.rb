0e04bd525cf801cbca89530334f77d824d925601
Dyf-Tfh <n.zhao@hotmail.fr>
2018-07-17 11:00:21 +0200

    rajout TODO plus on l'espère rien cassé

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,16 +2,23 @@ class BoardCase
 
   attr_accessor :value, :case_number
 
-  def initialize
+  def initialize(value = " ",case_number)
+    #TO DO doit régler sa valeur, ainsi que son numéro de case
+    @value = value
+    @case_number = case_number
   end
 
   def to_s
+    #TO DO : doit renvoyer la valeur au format string
+    value.to_s
   end
+
 end
 
 class Board
 
   include Enumerable
+  #TO DO : la classe a 1 attr_accessor, une array qui contient les BoardCases
 
   attr_accessor :cases
 
@@ -20,16 +27,20 @@ class Board
   end
 
   def to_s
+  #TO DO : afficher le plateau
+
   end
 
   def play
+    #TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
   end
 
   def victory?
+    #TO DO : qui gagne ?
   end
-end
 
 class Player
+  #TO DO : la classe a 2 attr_accessor, son nom, sa valeur (X ou O). Elle a un attr_writer : il a gagné ?
 
   attr_accessor :name, :value #Noms et valeurs
   attr_writer :state #Victoire ou défaite
@@ -41,19 +52,19 @@ class Player
 end
 
 class Game
-
-  def initialize #Création des joueurs et du plateau
-    puts "Welcome to the Tic-Tac-Toe game !"
-    @player1 = Player.new
-    @player2 = Player.new
-    @board = Board.new
+  def initialize
+    #TO DO : créé 2 joueurs, créé un board
   end
 
-  def go #Lancement de la partie
+  def go
+    # TO DO : lance la partie
   end
 
-  def turn #Pour passer les tours
+  def turn
+    #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
   end
+
+
 end
 
 Game.new.go
