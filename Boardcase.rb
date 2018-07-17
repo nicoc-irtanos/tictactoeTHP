06e404e3ddf0e71134d5e2046e1946172ad8be4c
Dyf-Tfh <n.zhao@hotmail.fr>
2018-07-17 18:57:40 +0200

    division en plusieurs fichiers

---
+++ Boardcase.rb
@@ -0,0 +1,12 @@
+class BoardCase
+  attr_accessor :value, :case_number
+
+  def initialize(case_number) #associe chaque case à son nombre (voir plus bas)
+    @value = case_number
+    @case_number = case_number
+  end
+
+  def to_s #Mise en place des value
+    value.to_s
+  end
+end
---
+++ Player.rb
@@ -0,0 +1,12 @@
+class Player
+  attr_accessor :name, :value # Noms et valeurs
+  attr_accessor :state # Victoire ou défaite
+
+  def initialize(value) #Laisse les joueurs mettre leurs noms, le state est vide au début, la value est imposée (voir plus bas)
+    puts "Set your name:"
+    @name = gets.chomp
+    @name = name
+    @value = value
+    @state = ""
+  end
+end
