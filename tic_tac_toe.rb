c4e645fb4d0dbc56a89076a2efa6ba364de33aa2
Dyf-Tfh <n.zhao@hotmail.fr>
2018-07-17 17:13:59 +0200

    ça marche

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,24 +1,25 @@
-class BoardCase
+# frozen_string_literal: true
 
+# frozen_string_literal: "true"
+
+class BoardCase
   attr_accessor :value, :case_number
 
   def initialize(case_number)
-    #TO DO doit régler sa valeur, ainsi que son numéro de case
+    # TO DO doit régler sa valeur, ainsi que son numéro de case
     @value = case_number
     @case_number = case_number
   end
 
   def to_s
-    #TO DO : doit renvoyer la valeur au format string
+    # TO DO : doit renvoyer la valeur au format string
     value.to_s
   end
-
 end
 
 class Board
-
   include Enumerable
-  #TO DO : la classe a 1 attr_accessor, une array qui contient les BoardCases
+  # TO DO : la classe a 1 attr_accessor, une array qui contient les BoardCases
 
   attr_accessor :cases
 
@@ -36,7 +37,7 @@ class Board
   end
 
   def to_s
-    #TO DO : afficher le plateau
+    # TO DO : afficher le plateau
     @cases.each do |board|
       board = board.to_s
     end
@@ -45,31 +46,50 @@ class Board
     puts @cases[2].join("|")
   end
 
-  def play(player,num)
-    #TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
-    if @cases.flatten[num-1].value == ("O")
-      puts "invalid move, player change"
-      turn
-    elsif @cases.flatten[num-1].value == ("X")
-      puts "invalid move, player change"
-    else @cases.flatten[num-1].value = player.value
+  def victory?(player1, player2)
+    win_combos = [[@boardcase_1, @boardcase_2, @boardcase_3],
+                  [@boardcase_1, @boardcase_4, @boardcase_7],
+                  [@boardcase_1, @boardcase_5, @boardcase_9],
+                  [@boardcase_4, @boardcase_5, @boardcase_6],
+                  [@boardcase_7, @boardcase_8, @boardcase_9],
+                  [@boardcase_3, @boardcase_5, @boardcase_7],
+                  [@boardcase_2, @boardcase_5, @boardcase_8],
+                  [@boardcase_3, @boardcase_6, @boardcase_9]]
+
+    win_combos.each do |combos|
+      if combos[0].to_s == player1.value && combos[1].to_s == player1.value && combos[2].to_s == player1.value
+        puts player1.name.class
+
+      elsif combos[0].to_s == player2.value && combos[1].to_s == player2.value && combos[2].to_s == player2.value
+        puts player2.name.class
+
+        # else
+        # puts "Tie! Enter q to quit game."
+      end
     end
   end
 
-  def victory?
-    #TO DO : qui gagne ?
+  def play(player, num)
+    # TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
+    if @cases.flatten[num - 1].value == "O"
+      puts "invalid move, player change"
+    elsif @cases.flatten[num - 1].value == "X"
+      puts "invalid move, player change"
+    else @cases.flatten[num - 1].value = player.value
+    end
   end
 end
 
 class Player
-  #TO DO : la classe a 2 attr_accessor, son nom, sa valeur (X ou O). Elle a un attr_writer : il a gagné ?
+  # TO DO : la classe a 2 attr_accessor, son nom, sa valeur (X ou O). Elle a un attr_writer : il a gagné ?
 
-  attr_accessor :name, :value #Noms et valeurs
-  attr_accessor :state #Victoire ou défaite
+  attr_accessor :name, :value # Noms et valeurs
+  attr_accessor :state # Victoire ou défaite
 
-  def initialize(value)
-    puts "Set your name:"
-    @name = gets.chomp
+  def initialize(name, value)
+    # puts "Set your name:"
+    # @name = gets.chomp
+    @name = name
     @value = value
     @state = ""
   end
@@ -77,9 +97,9 @@ end
 
 class Game
   def initialize
-    #TO DO : créé 2 joueurs, créé un board
-    @player1 = Player.new("X")
-    @player2 = Player.new("O")
+    # TO DO : créé 2 joueurs, créé un board
+    @player1 = Player.new("1", "X")
+    @player2 = Player.new("2", "O")
     @board = Board.new
   end
 
@@ -91,19 +111,15 @@ class Game
   end
 
   def turn
-    #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
-
-    #if board.victory?
-
-    #else
-
-    while (@player1.state == "" and @player2.state == "") do
-    puts @player1.name.to_s + "'s turn"
-    puts "Choose a case"
-    @board.play(@player1,gets.chomp.to_i)
-    @board.to_s
-    #switch les joueurs
-    @player1, @player2 = @player2, @player1
+    # TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
+    while (@player1.state == "") && (@player2.state = "")
+      puts "tour de :" + @player1.name.to_s
+      puts "choisisez une case"
+      @board.play(@player1, gets.chomp.to_i)
+      @board.to_s
+      # switch les joueurs
+      @board.victory?(@player1, @player2)
+      @player1, @player2 = @player2, @player1
     end
   end
 end
